46826983f67acf14fe77292d9daa6d91288e9689
dyushakolesov <dyushakolesov@mail.ru>
2022-03-28 21:20:30 +0300

    add plots

--- face_recognition2.py
+++ face_recognition2.py
@@ -7,8 +7,8 @@ import os
 import matplotlib.pyplot as plt
 import cv2
 
-DISPLAY_PARAM = 1 # display res images or return score
-CALCULATE_BEST = 0
+DISPLAY_PARAM = 0 # display res images or return score
+CALCULATE_BEST = 1
 PARALLEL_METHOD = 0 # when we use parallel method it is 0
 
 def load_images_from_folder(folder):
@@ -19,6 +19,23 @@ def load_images_from_folder(folder):
         if image is not None:
             images.append(image)
     return images
+def load_images_from_folders(folders_num,start_idx,train_len):
+    test_images = []
+    train_images = []
+    folder_base = "cross_val"
+    for folder_idx in range (folders_num):
+        folder = folder_base + "/" + str(folder_idx+1)
+        curr_idx = 0
+        for filename in os.listdir(folder):
+            image = skimage.io.imread(fname=os.path.join(folder,filename), as_gray=True)
+            image = cv2.imread(os.path.join(folder,filename), 0)
+            if image is not None:
+                if(curr_idx >= start_idx and curr_idx < start_idx + train_len):
+                    train_images.append(image)
+                else:
+                    test_images.append(image)
+            curr_idx += 1
+    return train_images, test_images
 def face_recognition_hist(BINS_NUM):
     
     scores =[]
@@ -381,6 +398,8 @@ from tkinter import filedialog
 from template_matching import *
 import tkinter
 import pathlib, os
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 
@@ -399,10 +418,11 @@ def openfolder(is_train):
 
 
 
-best_param_vec = [108,5.5,15,4,27]
+best_param_vec = [108,5.5,25,9,24]
 def calculate_res(train_path, test_path):
     global train_images
     global test_images
+    
     text = text_edit.get(0.3,END)
     print(text)
     try:
@@ -410,63 +430,81 @@ def calculate_res(train_path, test_path):
         label_param.config(text = "Write parameter value (now set as " + text + " )")
     except Exception:
         label_param.config(text = "Write parameter value (number only!):")
-    train_images = load_images_from_folder(train_path)
-    test_images = load_images_from_folder(test_path)
+    train_images, test_images = load_images_from_folders(folders_num=5,start_idx=0, train_len=2)
+    #train_images = load_images_from_folder(train_path)
+    #test_images = load_images_from_folder(test_path)
+    plot.clear()
     if(var.get() == 1):
         face_recognition_hist(BINS_NUM=param)
         if(CALCULATE_BEST):
+            
             best = MAXINT
             best_param = -1
+            scores = []
             for i in range(32,128,2):
                 curr_score = np.array(face_recognition_hist(BINS_NUM=i)).mean()
+                scores.append(curr_score)
                 if (curr_score < best):
                     best_param = i
                     best = curr_score
             label_param.config(text = "Best parameter value: " + str(best_param))
+            plot.plot(range(32,128,2), scores, color="blue", marker="o")
     elif(var.get() == 2):
         face_recognition_scale(scale_param=param)
         best = MAXINT
         best_param = -1
+        scores = []
         if(CALCULATE_BEST):
             for i in np.arange(2,6,0.5):
                 curr_score = np.array(face_recognition_scale(scale_param=i)).mean()
+                scores.append(curr_score)
                 if (curr_score < best):
                     best_param = i
                     best = curr_score
             label_param.config(text = "Best parameter value: " + str(best_param))
+            plot.plot(np.arange(2,6,0.5), scores, color="blue", marker="o")
     elif(var.get() == 3):
         face_recognition_dct(components_num=param)
         best = MAXINT
         best_param = -1
+        scores = []
         if(CALCULATE_BEST):
             for i in np.arange(4,32):
                 curr_score = np.array(face_recognition_dct(components_num=i)).mean()
+                scores.append(curr_score)
                 if (curr_score < best):
                     best_param = i
                     best = curr_score
             label_param.config(text = "Best parameter value: " + str(best_param))
+            plot.plot(np.arange(4,32), scores, color="blue", marker="o")
     elif(var.get() == 4):
         face_recognition_dft(components_num=param)
         best = MAXINT
         best_param = -1
+        scores = []
         if(CALCULATE_BEST):
             for i in np.arange(4,32):
                 curr_score = np.array(face_recognition_dft(components_num=i)).mean()
+                scores.append(curr_score)
                 if (curr_score < best):
                     best_param = i
                     best = curr_score
             label_param.config(text = "Best parameter value: " + str(best_param))
+            plot.plot(np.arange(4,32), scores, color="blue", marker="o")
     elif(var.get() == 5):
         face_recognition_grad(width=param)
         best = MAXINT
         best_param = -1
+        scores = []
         if(CALCULATE_BEST):
             for i in np.arange(1,30):
                 curr_score = np.array(face_recognition_grad(width=i)).mean()
+                scores.append(curr_score)
                 if (curr_score < best):
                     best_param = i
                     best = curr_score
             label_param.config(text = "Best parameter value: " + str(best_param))
+            plot.plot(np.arange(1,30), scores, color="blue", marker="o")
     else:
         global PARALLEL_METHOD
         PARALLEL_METHOD = 1
@@ -478,29 +516,29 @@ def calculate_res(train_path, test_path):
         result = mode([vec1,vec2,vec3,vec4,vec5])[0][0]
         print(result)
         #plotting result:
-    rows = 1
-    columns = 2
-    idx = 0
-    for elem in result:
-        fig, ax = plt.subplots()
-        fig.add_subplot(rows, columns, 1)
-        plt.draw()
-        # showing image
-        plt.imshow(test_images[idx],cmap='gray')
-        plt.axis('off')
-        plt.title("Test")
+        rows = 1
+        columns = 2
+        idx = 0
+        for elem in result:
+            fig, ax = plt.subplots()
+            fig.add_subplot(rows, columns, 1)
+            plt.draw()
+            # showing image
+            plt.imshow(test_images[idx],cmap='gray')
+            plt.axis('off')
+            plt.title("Test")
   
-        # Adds a subplot at the 2nd position
-        fig.add_subplot(rows, columns, 2)
-        plt.imshow(train_images[elem],cmap='gray')
-        plt.axis('off')
-        plt.title("Train voted")
-        plt.get_current_fig_manager().full_screen_toggle()
-        plt.show(block=False)
-        plt.pause(0.5)
-        plt.close()
-        idx += 1    
-    print("OK")
+            # Adds a subplot at the 2nd position
+            fig.add_subplot(rows, columns, 2)
+            plt.imshow(train_images[elem],cmap='gray')
+            plt.axis('off')
+            plt.title("Train voted")
+            plt.get_current_fig_manager().full_screen_toggle()
+            plt.show(block=False)
+            plt.pause(0.5)
+            plt.close()
+            idx += 1    
+        print("OK")
 
 
 
@@ -513,7 +551,9 @@ root.title("Image Loader")
 # Set the resolution of window
 root.geometry("550x300+300+150")
 
-
+#init plot
+figure = Figure(figsize=(4, 3), dpi=100)
+plot = figure.add_subplot(1, 1, 1)
 
 # Allow Window to be resizable
 root.resizable(width=True, height=True)
@@ -564,5 +604,7 @@ R4.grid(row=6,column=0)
 R5.grid(row=7,column=0)
 R6.grid(row=8,column=0)
 
-
+#set loaction of plot
+canvas = FigureCanvasTkAgg(figure, root)
+canvas.get_tk_widget().grid(row=8,column=4)
 root.mainloop()
